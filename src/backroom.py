603539663785bad82801cc1ac1c1a154a506e16f
Liam <liamgoesbrrr@gmail.com>
2026-06-27 23:37:57 +0900

    switchbig() line added

--- src/backroom.py
+++ src/backroom.py
@@ -6,16 +6,17 @@ noclip.initialize((1280, 720))
 def setupgame():
     global basespace
     basespace = noclip.space_001
-    img = noclip.loadimage("assets/bacteria.webp")
+    img = noclip.setopacity(noclip.loadimage("assets/bacteria.png"), 100)
+    img2 = noclip.setopacity(noclip.loadimage("assets/backrooms.jpg"), 80)
     x = noclip.centerX(basespace.width, img.width)
     y = noclip.centerY(basespace.height, img.height)
-    basespace.newchild(noclip.thing("shit2", (0, 0), None))
+    basespace.newchild(noclip.thing("shit2", (x, y), img2))
     basespace.newchild(noclip.thing("shit", (x, y), img))
+    noclip.switchbig("shit")
 
 
 def update():
-    s = basespace.returnchild("shit")
-    s.x = noclip.centerX(basespace.width, s.width)
-    s.y = noclip.centerY(basespace.height, s.height)
+    if noclip.keyPressed("space"):
+        noclip.switchbig("shit2")
 
 noclip.run(setupgame, update)
