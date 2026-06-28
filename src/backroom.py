50d76a169ea3f036a6dec8dfd6f98664a63666fe
Liam <liamgoesbrrr@gmail.com>
2026-06-28 15:06:40 +0900

    timer + cleaner code

--- src/backroom.py
+++ src/backroom.py
@@ -6,17 +6,24 @@ noclip.initialize((1280, 720))
 def setupgame():
     global basespace
     basespace = noclip.space_001
-    img = noclip.setopacity(noclip.loadimage("assets/bacteria.png"), 100)
-    img2 = noclip.setopacity(noclip.loadimage("assets/backrooms.jpg"), 80)
+    img = noclip.setopacity(noclip.loadimage("assets/backrooms.jpg"), 100)
+    img2 = noclip.setopacity(noclip.loadimage("assets/bacteria.png"), 80)
     x = noclip.centerX(basespace.width, img.width)
     y = noclip.centerY(basespace.height, img.height)
     basespace.newchild(noclip.thing("shit2", (x, y), img2))
+    basespace.returnchild("shit2").add_timer("flasheffect")
     basespace.newchild(noclip.thing("shit", (x, y), img))
-    noclip.switchbig("shit")
 
+    def shit2_update(self):
+        self.timer["flasheffect"] += noclip.dt
+        if self.timer["flasheffect"] > 0.3:
+            noclip.switchbig("shit")
+            self.timer["flasheffect"] = 0
+
+    basespace.returnchild("shit2").update = shit2_update
+    noclip.switchbig("shit")
 
 def update():
     if noclip.keyPressed("space"):
         noclip.switchbig("shit2")
-
 noclip.run(setupgame, update)
