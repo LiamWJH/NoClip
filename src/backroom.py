a8064331e1ecaa05694c370dc094763c83f8a99d
Liam <liamgoesbrrr@gmail.com>
2026-06-28 23:15:34 +0900

    audio + keypress stuff

--- src/backroom.py
+++ src/backroom.py
@@ -8,6 +8,7 @@ def setupgame():
     basespace = noclip.space_001
     noclip.assets.image("backrooms", fileformat="jpg")
     noclip.assets.image("bacteria", fileformat="png")
+    noclip.assets.sound("fah", fileformat="mp3")
     x = noclip.centerX(basespace.width, noclip.assets.image("backrooms").width)
     y = noclip.centerY(basespace.height, noclip.assets.image("bacteria").height)
     basespace.newchild(noclip.thing("shit2", (x, y), noclip.assets.image("bacteria")))
@@ -24,8 +25,9 @@ def setupgame():
     noclip.switchbig("shit")
 
 def update():
-    if noclip.keyPressed("space"):
+    if noclip.keyJustPressed("space"):
         noclip.switchbig("shit2")
+        noclip.assets.sound("fah", volume=0.01).play()
     if noclip.keyPressed("right"):
         noclip.camera.move_view_xy((90, 3))
     if noclip.keyPressed("left"):
